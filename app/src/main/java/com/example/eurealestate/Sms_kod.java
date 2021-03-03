df030d2002600090272dfc7334f4bc0d8eef7e9b
0XRA <code.0XRA@gmail.com>
2021-03-03 13:30:14 +0300

    2 page

--- app/src/main/java/com/example/eurealestate/Sms_kod.java
+++ app/src/main/java/com/example/eurealestate/Sms_kod.java
@@ -1,17 +1,34 @@
 package com.example.eurealestate;
 
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import com.hanks.htextview.fall.FallTextView;
+import com.royrodriguez.transitionbutton.TransitionButton;
+
+import static android.view.View.VISIBLE;
+import static android.view.View.INVISIBLE;
 
 
 public class Sms_kod extends AppCompatActivity {
 
     FallTextView textView;
     int schet=0;
+    TransitionButton start;
+    LinearLayout ramka;
+    RadioButton chast, agenst, komp,pokup;
+    RadioGroup radioGroup;
+    boolean flag=false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +38,57 @@ public class Sms_kod extends AppCompatActivity {
         textView = findViewById(R.id.fadetextview);
         textView.animateText("Добро пожаловать!");
         textView.setOnClickListener(onClickListener);
+        start = findViewById(R.id.Start_button);
+        start.setOnClickListener(onClickListener);
+        ramka = findViewById(R.id.ramka);
+        start.isEnabled();
 
+        chast = findViewById(R.id.checkbox_chast);
+        agenst = findViewById(R.id.checkbox_agenstvo);
+        komp = findViewById(R.id.checkbox_kompaniya);
+        pokup = findViewById(R.id.checkbox_pokupatel);
+
+        radioGroup = findViewById(R.id.radiogroup);
+
+        chast.setOnClickListener(onClickListener);
+        agenst.setOnClickListener(onClickListener);
+        komp.setOnClickListener(onClickListener);
+        pokup.setOnClickListener(onClickListener);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                SharedPreferences tip_klienta = getSharedPreferences("tip", MODE_PRIVATE);
+                SharedPreferences.Editor editor = tip_klienta.edit();
+                switch (checkedId){
+                    case R.id.checkbox_chast:
+                        start.animate().alpha(1.0f);
+                        flag=true;
+                        editor.putString("tip","chast");
+                        editor.apply();
+                        break;
+                    case R.id.checkbox_agenstvo:
+                        start.animate().alpha(1.0f);
+                        flag=true;
+                        editor.putString("tip","agenst");
+                        editor.apply();
+                        break;
+                    case R.id.checkbox_kompaniya:
+                        start.animate().alpha(1.0f);
+                        editor.putString("tip","chast");
+                        editor.apply();
+                        flag=true;
+                        break;
+                    case R.id.checkbox_pokupatel:
+                        start.animate().alpha(1.0f);
+                        flag=true;
+                        editor.putString("tip","pokup");
+                        editor.apply();
+                        break;
+
+                }
+            }
+        });
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -52,22 +119,45 @@ public class Sms_kod extends AppCompatActivity {
                             textView.animateText("Enjoy");
                             break;
                         case 6:
+                            textView.animate().alpha(0.0f);
                             textView.animateText(" ");
+                            textView.setEnabled(false);
                             Runnable r = new Runnable() {
                                 @Override
                                 public void run() {
-                                    textView.setVisibility(View.INVISIBLE);
 
+                                    start.setVisibility(VISIBLE);
+                                    ramka.animate().alpha(0.0f);
+                                    start.animate().alpha(1.0f);
                                 }
                             };
                             Handler h = new Handler();
                             h.postDelayed(r,1000);
+                            schet=0;
                             break;
                     }
                 break;
 
+                case R.id.Start_button:
+                    if(flag){
+                        Intent intent =new Intent(Sms_kod.this,regist.class);
+                        startActivity(intent);
+                    }else{
+                        start.animate().alpha(0.0f);
+                        start.setVisibility(INVISIBLE);
+                        start.setText("Готово");
+                        start.setVisibility(VISIBLE);
+                        start.animate().alpha(0.5f);
+                        ramka.setVisibility(VISIBLE);
+                        ramka.animate().alpha(1.0f);
+                    }
+                    break;
 
             }
         }
     };
+
+
+
+
 }

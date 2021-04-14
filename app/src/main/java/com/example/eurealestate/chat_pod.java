b97d11641f831a4090112f413841a0b44e5c6365
0XRA <code.0XRA@gmail.com>
2021-04-14 13:43:00 +0300

    chat_pod

--- app/src/main/java/com/example/eurealestate/chat_pod.java
+++ app/src/main/java/com/example/eurealestate/chat_pod.java
@@ -1,11 +1,13 @@
 package com.example.eurealestate;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class chat_pod extends AppCompatActivity {
 
@@ -18,6 +20,12 @@ public class chat_pod extends AppCompatActivity {
         setContentView(R.layout.activity_chat_pod);
         bnm=findViewById(R.id.bnm);
         bnm.setSelectedItemId(R.id.chat_sel);
+
+        String[] users = { "Ваша проблема", "Как сменить номер телефона", "Не прогружается VR-шоурум", "Не подключатся VR-очки"};
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
 
         bnm.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
